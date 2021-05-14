c0aa9a00ebdadb5955b18fd74c5d8bddfbf221dc
Avin-Techv <avin@techversantinfo.com>
2021-05-14 12:19:36 +0530

    payment admin page tweaks

--- film_hobo/payment/serializers.py
+++ film_hobo/payment/serializers.py
@@ -4,7 +4,16 @@ from hobo_user.models import PromoCode
 
 
 class DiscountsSerializer(serializers.ModelSerializer):
+    valid_from = serializers.SerializerMethodField(source='get_valid_from')
+    valid_to = serializers.SerializerMethodField(source='get_valid_to')
+
     class Meta:
         model = PromoCode
         fields = ['id', 'promo_code', 'created_time', 'valid_from', 'valid_to',
                   'life_span', 'amount_type', 'amount', 'user_type']
+
+    def get_valid_from(self, obj):
+        return obj.valid_from.date()
+
+    def get_valid_to(self, obj):
+        return obj.valid_to.date()
