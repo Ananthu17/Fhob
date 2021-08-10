cf9091834523a25626dd1c244978c8cb5b6dc02c
Avin-Techv <avin@techversantinfo.com>
2021-08-10 11:49:23 +0530

    jquery tweak done

--- film_hobo/static/js/screening_video_page.js
+++ film_hobo/static/js/screening_video_page.js
@@ -9,16 +9,21 @@ $('#share-link').click(function(event){
 
 var token = $("#token")
 var token_auth_str = token.val()
+var validUsers = [];
+var validEmails = [];
 
 get_friends_list_url = origin_url + '/hobo_user/list-all-friend-api/'
 axios.get(get_friends_list_url, {headers: { 'Authorization': token_auth_str }})
 .then((response) => {
     var friends_obj = response.data.friends
-    var validOptions = [];
+
     $.each(friends_obj, function(key_1,valueObj_1){
         $.each(valueObj_1, function(key_2,valueObj_2){
             if (key_2 === "user"){
-                validOptions.push(valueObj_2);
+                validUsers.push(valueObj_2);
+            }
+            if (key_2 === "email"){
+                validEmails.push(valueObj_2);
             }
         });
     });
@@ -26,11 +31,11 @@ axios.get(get_friends_list_url, {headers: { 'Authorization': token_auth_str }})
     previousValue = "";
 
     $('#enter_email').autocomplete({
-        source: validOptions
+        source: validUsers
     }).keyup(function() {
         var isValid = false;
-        for (i in validOptions) {
-            if (validOptions[i].toLowerCase().match(this.value.toLowerCase())) {
+        for (i in validUsers) {
+            if (validUsers[i].toLowerCase().match(this.value.toLowerCase())) {
                 isValid = true;
             }
         }
@@ -46,28 +51,33 @@ axios.get(get_friends_list_url, {headers: { 'Authorization': token_auth_str }})
 });
 
 $('#invite_send').click(function(event){
+
     var email = $("#all_mail")
-    for ( var i = 0, l = email.length; i < l; i++ ) {
-        var to_send_email = email.children()[i].innerText.slice(0,-2)
-        console.log(to_send_email)
+    var email_list = validEmails
+    // for ( var i = 0, l = email.children().length; i < l; i++ ) {
+    //     var to_send_email = email.children()[i].innerText.slice(0,-2)
+    //     email_list.push(to_send_email)
+    // }
+
+    console.log(email_list)
+    for ( var i = 0, l = email_list.length; i < l; i++ ) {
+        var url_to_share = window.location.origin + window.location.pathname
+
+        invite_url = window.location.origin + '/hobo_user/user_home/invite/'
+        invite_url_args = {
+            "to_user_email": email_list[i],
+            "project_url": url_to_share,
+        }
+
+        axios.post(invite_url, invite_url_args, {headers: { 'Authorization': token_auth_str }})
+        .then((response) => {
+            debugger
+            console.log(response);
+        }, (error) => {
+            debugger
+            console.log(error);
+        });
     }
-    // var to_send_email = email.children()[0].innerText.slice(0,-2)
-    var url_to_share = window.location.origin + window.location.pathname
-
-    invite_url = window.location.origin + '/hobo_user/user_home/invite/'
-
-    invite_url_args = {
-        "to_user_email": to_send_email,
-        "project_url": url_to_share,
-    }
-
-    axios.post(invite_url, invite_url_args, {headers: { 'Authorization': token_auth_str }})
-    .then((response) => {
-        console.log(response);
-    }, (error) => {
-        console.log(error);
-    });
-
 });
 
 $('#share-close-btn').click(function(event){
