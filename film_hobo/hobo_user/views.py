2e26202363351ee25a4d2c83123eca727ea93d0c
Avin-Techv <avin@techversantinfo.com>
2021-09-16 10:31:23 +0530

    added json for free trail

--- film_hobo/hobo_user/views.py
+++ film_hobo/hobo_user/views.py
@@ -24,7 +24,6 @@ from django.contrib.auth.views import LogoutView as DjangoLogout
 from django.shortcuts import get_object_or_404, render, redirect
 from django.views.generic import TemplateView, View, FormView
 from django.contrib.auth.mixins import LoginRequiredMixin
-# from django_filters import rest_framework as filters
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticated, AllowAny
 
@@ -4498,63 +4497,54 @@ class AddBetaTesterCode(APIView):
             else:
                 pass
 
-            create_plan_json = {
-                "product_id": settings.PRODUCT_ID,
+            create_plan_json ={
                 "name": plan_name,
                 "description": plan_name,
-                "status": "ACTIVE",
+                "product_id": settings.PRODUCT_ID,
                 "billing_cycles": [
-                {
-                    "frequency": {
-                        "interval_unit": "DAY",
-                        "interval_count": request.data['days']
+                    {
+                        "frequency": {
+                            "interval_unit": "DAY",
+                            "interval_count": request.data['days']
+                        },
+                        "tenure_type": "TRIAL",
+                        "sequence": 1,
+                        "total_cycles": 1,
+                        "pricing_scheme": {
+                            "fixed_price": {
+                                "value": "0",
+                                "currency_code": "USD"
+                            }
+                        }
                     },
-                    "tenure_type": "TRIAL",
-                    "sequence": 1,
-                    "total_cycles": 1,
-                    "pricing_scheme": {
-                        "fixed_price": {
-                            "value": "0",
-                            "currency_code": "USD"
+                    {
+                        "frequency": {
+                            "interval_unit": plan_interval_unit,
+                            "interval_count": 1
+                        },
+                        "tenure_type": "REGULAR",
+                        "sequence": 2,
+                        "total_cycles": 0,
+                        "pricing_scheme": {
+                            "fixed_price": {
+                                "value": plan_interval_count,
+                                "currency_code": "USD"
+                            }
                         }
                     }
-                },
-                {
-                    "frequency": {
-                        "interval_unit": plan_interval_unit,
-                        "interval_count": plan_interval_count
-                    },
-                    "tenure_type": "REGULAR",
-                    "sequence": 2,
-                    "total_cycles": 0,
-                    "pricing_scheme": {
-                        "fixed_price": {
-                            "value": "15",
-                            "currency_code": "USD"
-                        }
-                    }
-                }
-            ],
+                ],
                 "payment_preferences": {
                     "auto_bill_outstanding": True,
-                    "setup_fee": {
-                    "value": "0",
-                    "currency_code": "USD"
-                    },
-                    "setup_fee_failure_action": "CONTINUE",
-                    "payment_failure_threshold": 3
-                },
-                "taxes": {
-                    "percentage": "0",
-                    "inclusive": False
+                    "payment_failure_threshold": 1
                 }
             }
-        import pdb;pdb.set_trace()
         create_plan_user_response = requests.post(
                         'https://api-m.sandbox.paypal.com/v1/billing/plans',
                         data=json.dumps(create_plan_json),
-                        headers={'Content-type': 'application/json',
-                                 'Authorization': access_token_strting})
+                        headers={'Accept': 'application/json',
+                                 'Authorization': access_token_strting,
+                                 'Content-type': 'application/json'
+                                 })
         plan_ids = []
         if create_plan_user_response.status_code == 201:
             plan_id = json.loads(create_plan_user_response.content)['id']
