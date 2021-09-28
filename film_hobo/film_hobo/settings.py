c1dbb1aeec4c13f35a59e047048b602596562305
Avin-Techv <avin@techversantinfo.com>
2021-09-28 17:25:00 +0530

    updated the database

--- film_hobo/film_hobo/settings.py
+++ film_hobo/film_hobo/settings.py
@@ -163,6 +163,18 @@ ASGI_APPLICATION = "film_hobo.asgi.application"
 # }
 
 # local database credentials
+# DATABASES = {
+#     'default': {
+#         'ENGINE': env("DATABASE_ENGINE"),
+#         'NAME': env("DATABASE_NAME"),
+#         'USER': env("DATABASE_USER"),
+#         'PASSWORD': env("DATABASE_PASSWORD"),
+#         'HOST': env("DATABASE_HOST"),
+#         'PORT': env("DATABASE_PORT"),
+#     }
+# }
+
+# demo server database credentials
 DATABASES = {
     'default': {
         'ENGINE': env("DATABASE_ENGINE"),
@@ -173,17 +185,6 @@ DATABASES = {
         'PORT': env("DATABASE_PORT"),
     }
 }
-
-# demo server database credentials
-# DATABASES = {
-#   'default': {
-#     'ENGINE': 'django.db.backends.postgresql',
-#     'HOST': os.environ.get('DB_HOST'),
-#     'NAME': os.environ.get('DB_NAME'),
-#     'USER': os.environ.get('DB_USER'),
-#     'PASSWORD': os.environ.get('DB_PASS'),
-#   }
-# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
