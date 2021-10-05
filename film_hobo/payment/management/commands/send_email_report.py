f3af9302e17da769d16eb2a2ed1ca2bbef6365ef
Avin-Techv <avin@techversantinfo.com>
2021-10-05 17:07:56 +0530

    update the new send routine

--- film_hobo/payment/management/commands/send_email_report.py
+++ film_hobo/payment/management/commands/send_email_report.py
@@ -1,9 +1,11 @@
+from datetime import timedelta
 import requests
 
 from django.core.management.base import BaseCommand
 from django.conf import settings
 from django.utils import timezone
 
+from hobo_user.models import CustomUser
 from payment.models import Transaction, ScheduledEmail
 
 
@@ -27,10 +29,23 @@ class Command(BaseCommand):
                                         data=data,
                                         headers={'Accept': 'application/json'})
                     if sheduled_remainder_email_response.status_code == 200:
-                        self.stdout.write("E-mail Reaminders were sent.")
+                        user_obj = CustomUser.objects.get(
+                            email=sheduled_emails_obj.user_email)
+                        sheduled_emails_obj.last_run_at = current_time
+                        if (user_obj.payment_plan == "monthly"):
+                            next_run_at = current_time + timedelta(days=30)
+                        else:
+                            next_run_at = current_time + timedelta(days=365)
+                        sheduled_emails_obj.next_run_at = next_run_at
+                        sheduled_emails_obj.save()
+                        self.stdout.write(
+                            "E-mail Reaminders were sent.")
                     else:
-                        self.stdout.write("Failure in sending remainder email.")
+                        self.stdout.write(
+                            "Failure in sending remainder email.")
                 else:
-                    self.stdout.write("No E-mail Reaminders to send.")
+                    self.stdout.write(
+                        "No E-mail Reaminders to send.")
         else:
-            self.stdout.write("No E-mail Reaminders to send.")
+            self.stdout.write(
+                "No E-mail Reaminders to send.")
