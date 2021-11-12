18872125657d85670a047b44199b5f4749c25086
developer-tvc <developer@techversantinfo.com>
2021-11-12 09:19:35 +0530

    company profile-made website optional

--- film_hobo/hobo_user/views.py
+++ film_hobo/hobo_user/views.py
@@ -2344,7 +2344,10 @@ class ProductionCompanyProfileAPI(APIView):
             if 'company_name' in data_dict:
                 user.company_name = data_dict['company_name']
             if 'company_website' in data_dict:
-                user.company_website = data_dict['company_website']
+                if data_dict['company_website'] == 'NONE':
+                    user.company_website = ''
+                else:
+                    user.company_website = data_dict['company_website']
             user.save()
             if 'imdb' in data_dict:
                 profile.imdb = data_dict['imdb']
@@ -2395,10 +2398,14 @@ class AgencyManagementCompanyProfileAPI(APIView):
         response = {}
         if serializer.is_valid():
             data_dict = serializer.data
+            print(data_dict)
             if 'company_name' in data_dict:
                 user.company_name = data_dict['company_name']
             if 'company_website' in data_dict:
-                user.company_website = data_dict['company_website']
+                if data_dict['company_website'] == 'NONE':
+                    user.company_website = ''
+                else:
+                    user.company_website = data_dict['company_website']
             if 'agency_management_type' in data_dict:
                 user.agency_management_type = data_dict[
                                 'agency_management_type']
@@ -2740,6 +2747,8 @@ class EditProductionCompanyView(LoginRequiredMixin, TemplateView):
         profile = CompanyProfile.objects.get(user=user)
         json_response = json.dumps(request.POST)
         json_dict = ast.literal_eval(json_response)
+        if json_dict['company_website'] == '':
+            json_dict['company_website'] = "NONE"
         if 'submission_policy_SAMR' in json_dict:
             submission_policy_SAMR = json_dict['submission_policy_SAMR']
             if(submission_policy_SAMR == 'members_with_rating' and 'rate' in json_dict):
@@ -2865,6 +2874,8 @@ class EditAgencyManagementCompanyView(LoginRequiredMixin, TemplateView):
         profile = CompanyProfile.objects.get(user=user)
         json_response = json.dumps(request.POST)
         json_dict = ast.literal_eval(json_response)
+        if json_dict['company_website'] == '':
+            json_dict['company_website'] = "NONE"
         if 'submission_policy_SAMR' in json_dict:
             submission_policy_SAMR = json_dict['submission_policy_SAMR']
             if(submission_policy_SAMR == 'members_with_rating' and 'rate' in json_dict):
@@ -3455,7 +3466,6 @@ class RateCompanyAPI(APIView):
     permission_classes = (IsAuthenticated,)
 
     def post(self, request):
-        print("here----------------------")
         serializer = self.serializer_class(data=request.data)
         response = {}
         if serializer.is_valid():
